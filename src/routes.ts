5c2e988242928a2da3ce7e7c015517690d825671
Jannis Baum <jannis.baum@gmail.com>
2023-07-19 19:01:19 +0200

    feat: serve updated files

--- src/routes.ts
+++ src/routes.ts
@@ -8,29 +8,39 @@ export const router = Router()
 const converter = new Converter();
 converter.setFlavor('github');
 
+const liveContent = new Map<string, string>()
+
 router.get(/.*/, async (req: Request, res: Response) => {
-    try {
-        const content = readFileSync(req.path).toString();
-        res.send(`
-<html>
-    <body>
-        ${converter.makeHtml(content)}
-    </body>
-    <script>
-        window.MKPV_PORT = "${process.env['PORT']}"
-        window.MKPV_PATH = "${req.path}"
-    </script>
-    <script type="text/javascript" src="/static/client.js"></script>
-</html>
-        `);
-    } catch {
-        res.status(404).send('File not found.');
+    const path = req.path;
+    let body = liveContent.get(path);
+    if (!body) {
+        try {
+            const content = readFileSync(req.path).toString();
+            body = converter.makeHtml(content)
+        } catch {
+            res.status(404).send('File not found.');
+            return;
+        }
     }
+    res.send(`
+        <html>
+            <body>${body}</body>
+            <script>
+                window.MKPV_PORT = "${process.env['PORT']}";
+                window.MKPV_PATH = "${req.path}";
+            </script>
+            <script type="text/javascript" src="/static/client.js"></script>
+        </html>
+    `);
 })
 
 router.post(/.*/, async (req: Request, res: Response) => {
     const path = req.path;
     const { content, cursor } = req.body;
-    messageClientsAt(path, content);
+
+    const parsed = converter.makeHtml(content);
+    liveContent.set(path, parsed);
+
+    messageClientsAt(path, `UPDATE: ${parsed}`);
     res.end();
 })
