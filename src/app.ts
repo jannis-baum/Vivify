47b3fc235e429397e141204840ab89bd4f318042
Jannis Baum <jannis.baum@gmail.com>
2023-07-19 11:40:16 +0200

    feat: read files

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
 import express, { Application , Request, Response} from 'express';
+import { readFileSync } from 'fs';
 
 const app: Application = express()
 
 const port: number = 31622
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello world!')
+app.get(/.*/, async (req: Request, res: Response) => {
+    try {
+        const content = readFileSync(req.path).toString();
+        res.send(content)
+    } catch {
+        res.status(400);
+        res.send('File not found.');
+    }
 })
 
 app.listen(port, function () {
-  console.log(`App is listening on port ${port} !`)
+    console.log(`App is listening on port ${port} !`)
 })
