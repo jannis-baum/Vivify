37dc3b0bd89de8bd382a3cef69b81a91913e87ad
Jannis Baum <jannis.baum@gmail.com>
2023-07-23 10:35:27 +0200

    feat: health client check

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,7 +1,14 @@
 import { Request, Response, Router } from "express";
 
+import { clientsAt } from "../app";
+
 export const router = Router()
 
 router.get('/', async (_: Request, res: Response) => {
     res.end();
+});
+
+router.get(/.+/, async (_: Request, res: Response) => {
+    if (clientsAt(res.locals.filepath).length) res.end();
+    else res.status(404).send('No clients');
 });
