4cd651667861d6665607e2e96c66892129f76d85
Jannis Baum <jannis.baum@gmail.com>
2023-07-24 10:37:50 +0200

    feat: inject line numbers

--- src/mdit.ts
+++ src/mdit.ts
@@ -1,13 +1,12 @@
 import MarkdownIt from "markdown-it";
 import anchor from "markdown-it-anchor";
-import emoji from "markdown-it-emoji";
-var taskLists = require("markdown-it-task-lists");
 
 const mdit = new MarkdownIt();
 mdit.use(anchor, { permalink: anchor.permalink.ariaHidden({
     placement: 'before'
 }) });
-mdit.use(emoji);
-mdit.use(taskLists);
+mdit.use(require("markdown-it-emoji"));
+mdit.use(require("markdown-it-task-lists"));
+mdit.use(require("markdown-it-inject-linenumbers"));
 
 export default mdit;
