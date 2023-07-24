4bbbf0181361a88b358ca7931b235ab1b2de9e3f
Jannis Baum <jannis.baum@gmail.com>
2023-07-24 10:33:11 +0200

    feat: task-lists

--- src/mdit.ts
+++ src/mdit.ts
@@ -1,11 +1,13 @@
 import MarkdownIt from "markdown-it";
 import anchor from "markdown-it-anchor";
 import emoji from "markdown-it-emoji";
+var taskLists = require("markdown-it-task-lists");
 
 const mdit = new MarkdownIt();
 mdit.use(anchor, { permalink: anchor.permalink.ariaHidden({
     placement: 'before'
 }) });
 mdit.use(emoji);
+mdit.use(taskLists);
 
 export default mdit;
