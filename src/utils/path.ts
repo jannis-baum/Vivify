820d75084e4624b262a0990a77fc9bbbc007e11f
Jannis Baum <jannis.baum@gmail.com>
2023-11-19 12:24:50 +0100

    feat(#26): path/url coding utils

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,4 +1,5 @@
 import { execSync } from 'child_process';
+import { homedir } from 'os';
 import { basename as pbasename, dirname as pdirname, parse as pparse } from 'path';
 
 export const pmime = (path: string) => execSync(`file --mime-type -b '${path}'`).toString().trim();
@@ -18,3 +19,12 @@ export const pcomponents = (path: string) => {
     if (parsed.base !== '') components.push(parsed.base);
     return components;
 };
+
+export const urlToPath = (url: string) => {
+    const encodedPath = url.replace(/^\/(viewer|health)\//, '');
+    const path = decodeURIComponent(encodedPath).replace(/^~/, homedir()).replace(/\/+$/, '');
+    return path === '' ? '/' : path;
+};
+
+export const pathToURL = (path: string, route: string = 'viewer') =>
+    `/${route}/${encodeURIComponent(path)}`;
