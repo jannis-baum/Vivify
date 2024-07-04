b7500f8098697d10a15c73a32af8461bd7ea1578
Jannis Baum <jannis.baum@gmail.com>
2024-07-04 13:39:02 +0200

    feat(#60): detect SEA and switch provision method

--- src/routes/static.ts
+++ src/routes/static.ts
@@ -2,6 +2,10 @@ import { Router } from 'express';
 import { readFile } from 'fs/promises';
 import { dirname as pdirname, join as pjoin } from 'path';
 
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { isSea } = require('node:sea');
+/* eslint-enable @typescript-eslint/no-var-requires */
+
 export const router = Router();
 
 class StaticProvider {
@@ -10,12 +14,20 @@ class StaticProvider {
     private _content: (path: string) => Promise<Buffer>;
 
     private constructor() {
-        this._content = (path) =>
-            this._contentDev(pjoin(pdirname(pdirname(__dirname)), 'static'), path);
+        if (isSea()) {
+            this._content = this._contentProd;
+        } else {
+            this._content = (path) =>
+                this._contentDev(pjoin(pdirname(pdirname(__dirname)), 'static'), path);
+        }
     }
 
     private async _contentDev(staticDir: string, path: string): Promise<Buffer> {
         return readFile(pjoin(staticDir, path));
+    }
+
+    private async _contentProd(path: string): Promise<Buffer> {
+        throw new Error('Not implemented' + path);
     }
 
     static async content(path: string): Promise<Buffer> {
