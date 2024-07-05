d4a0e42d5eb7167b0aec032688177955b19d3f06
Jannis Baum <jannis.baum@gmail.com>
2024-07-05 20:13:28 +0200

    feat(#55): markdown cells

--- src/parser/ipynb.ts
+++ src/parser/ipynb.ts
@@ -6,12 +6,21 @@ const renderNotebook: Renderer = (content: string): string => {
     const nb = JSON.parse(content) as INotebookContent;
     const language = nb.metadata.kernelspec?.language ?? nb.metadata.language_info?.name;
 
-    function joinMultilineString(str: MultilineString): string {
-        return Array.isArray(str) ? str.join('') : str;
-    }
-
     function renderCell(cell: ICell): string {
-        return renderMarkdown(`\`\`\`${language}\n${joinMultilineString(cell.source)}\n\`\`\``);
+        function joinMultilineString(str: MultilineString): string {
+            return Array.isArray(str) ? str.join('') : str;
+        }
+        const source = joinMultilineString(cell.source);
+
+        switch (cell.cell_type) {
+            case 'code':
+                return renderMarkdown(`\`\`\`${language}\n${source}\n\`\`\``);
+            case 'markdown':
+                return renderMarkdown(source);
+            default:
+                // 'raw' cells aren't normally rendered
+                return '';
+        }
     }
 
     return nb.cells.map(renderCell).join('\n\n');
