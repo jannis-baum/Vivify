14f538c34599cc467e953ee7bc3e2eac07c0e00a
Jannis Baum <jannis.baum@gmail.com>
2024-07-05 20:06:09 +0200

    feat(#55): get notebook language

--- src/parser/ipynb.ts
+++ src/parser/ipynb.ts
@@ -2,16 +2,18 @@ import { ICell, INotebookContent, MultilineString } from '@jupyterlab/nbformat';
 import renderMarkdown from './markdown';
 import { Renderer } from './parser';
 
-function joinMultilineString(str: MultilineString): string {
-    return Array.isArray(str) ? str.join('') : str;
-}
-
-function renderCell(cell: ICell): string {
-    return renderMarkdown(`\`\`\`\n${joinMultilineString(cell.source)}\n\`\`\``);
-}
-
 const renderNotebook: Renderer = (content: string): string => {
     const nb = JSON.parse(content) as INotebookContent;
+    const language = nb.metadata.kernelspec?.language ?? nb.metadata.language_info?.name;
+
+    function joinMultilineString(str: MultilineString): string {
+        return Array.isArray(str) ? str.join('') : str;
+    }
+
+    function renderCell(cell: ICell): string {
+        return renderMarkdown(`\`\`\`${language}\n${joinMultilineString(cell.source)}\n\`\`\``);
+    }
+
     return nb.cells.map(renderCell).join('\n\n');
 };
 export default renderNotebook;
