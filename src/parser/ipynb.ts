bc3589a0b32d036ca9f782de9ebd517629ad3e38
Jannis Baum <jannis.baum@gmail.com>
2024-07-06 13:31:03 +0200

    feat(#55): wrap cells in container

--- src/parser/ipynb.ts
+++ src/parser/ipynb.ts
@@ -2,21 +2,34 @@ import { ICell, INotebookContent, MultilineString } from '@jupyterlab/nbformat';
 import renderMarkdown from './markdown';
 import { Renderer } from './parser';
 
+function joinMultilineString(str: MultilineString): string {
+    return Array.isArray(str) ? str.join('') : str;
+}
+
+function contain(content: string, classNames: string | string[], tag: string = 'div'): string {
+    function prefix(classNames: string | string[]): string {
+        if (Array.isArray(classNames)) {
+            return classNames.map((c) => prefix(c)).join(' ');
+        }
+        return `ipynb-${classNames}`;
+    }
+    classNames = prefix(classNames);
+
+    return `<${tag} class="${classNames}">${content}</${tag}>`;
+}
+
 const renderNotebook: Renderer = (content: string): string => {
     const nb = JSON.parse(content) as INotebookContent;
     const language = nb.metadata.kernelspec?.language ?? nb.metadata.language_info?.name;
 
     function renderCell(cell: ICell): string {
-        function joinMultilineString(str: MultilineString): string {
-            return Array.isArray(str) ? str.join('') : str;
-        }
         const source = joinMultilineString(cell.source);
 
         switch (cell.cell_type) {
             case 'code':
-                return renderMarkdown(`\`\`\`${language}\n${source}\n\`\`\``);
+                return contain(renderMarkdown(`\`\`\`${language}\n${source}\n\`\`\``), 'cell-code');
             case 'markdown':
-                return renderMarkdown(source);
+                return contain(renderMarkdown(source), 'cell-markdown');
             default:
                 // 'raw' cells aren't normally rendered
                 return '';
