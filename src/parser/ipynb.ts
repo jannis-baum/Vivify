e570fdd1581e7a843960150292ed368f23292165
Jannis Baum <jannis.baum@gmail.com>
2024-07-06 16:32:26 +0200

    feat(#55): stream outputs with ansi

--- src/parser/ipynb.ts
+++ src/parser/ipynb.ts
@@ -1,4 +1,12 @@
-import { ICell, INotebookContent, MultilineString } from '@jupyterlab/nbformat';
+import {
+    ICell,
+    ICodeCell,
+    INotebookContent,
+    IOutput,
+    IStream,
+    MultilineString,
+} from '@jupyterlab/nbformat';
+import renderAnsi from './ansi';
 import renderMarkdown from './markdown';
 import { Renderer } from './parser';
 
@@ -26,19 +34,33 @@ const renderNotebook: Renderer = (content: string): string => {
     const nb = JSON.parse(content) as INotebookContent;
     const language = nb.metadata.kernelspec?.language ?? nb.metadata.language_info?.name;
 
+    function renderOutput(output: IOutput): string {
+        switch (output.output_type) {
+            case 'stream':
+                const text = joinMultilineString((output as IStream).text);
+                return contain(renderAnsi(text), 'output-stream', 'pre');
+            case 'error':
+            case 'execute_result':
+            case 'display_data':
+            default:
+                return '';
+        }
+    }
+
     function renderCell(cell: ICell): string {
         const source = joinMultilineString(cell.source);
 
         switch (cell.cell_type) {
             case 'code':
                 const executionCount = cell.execution_count?.toString() ?? ' ';
-                return contain(
-                    [
-                        contain(`In [${executionCount}]:`, 'execution-count'),
-                        renderMarkdown(`\`\`\`${language}\n${source}\n\`\`\``),
-                    ],
-                    'cell-code',
-                );
+                const content = [
+                    contain(`In [${executionCount}]:`, 'execution-count'),
+                    renderMarkdown(`\`\`\`${language}\n${source}\n\`\`\``),
+                ];
+                if (Array.isArray(cell.outputs)) {
+                    content.push(...(cell as ICodeCell).outputs.map(renderOutput));
+                }
+                return contain(content, 'cell-code');
             case 'markdown':
                 return contain(renderMarkdown(source), 'cell-markdown');
             default:
