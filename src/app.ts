0beb1e8317494657398f2cd009fb13a6bf66c911
Jannis Baum <jannis.baum@gmail.com>
2024-07-12 09:59:42 +0200

    feat(#90): run open through node

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
-import { createServer } from 'http';
+import { createServer, get } from 'http';
+import { resolve as presolve } from 'path';
 
 import express from 'express';
+import open from 'open';
 
-import { router as healthRouter } from './routes/health.js';
-import { router as viewerRouter } from './routes/viewer.js';
-import { router as staticRouter } from './routes/static.js';
-import { setupSockets } from './sockets.js';
-import { urlToPath } from './utils/path.js';
 import config from './parser/config.js';
+import { router as healthRouter } from './routes/health.js';
+import { router as staticRouter } from './routes/static.js';
+import { router as viewerRouter } from './routes/viewer.js';
+import { setupSockets } from './sockets.js';
+import { pathToURL, urlToPath } from './utils/path.js';
+import { existsSync } from 'fs';
 
 const app = express();
 app.use(express.json());
@@ -20,10 +23,6 @@ app.use('/health', healthRouter);
 app.use('/viewer', viewerRouter);
 
 const server = createServer(app);
-
-server.listen(config.port, async () => {
-    console.log(`App is listening on port ${config.port}!`);
-});
 
 let shutdownTimer: NodeJS.Timeout | null = null;
 export const { clientsAt, messageClientsAt } = setupSockets(
@@ -39,3 +38,29 @@ export const { clientsAt, messageClientsAt } = setupSockets(
         if (shutdownTimer) clearInterval(shutdownTimer);
     },
 );
+
+const address = `http://localhost:${config.port}`;
+const openArgs = () => {
+    for (const path of process.argv.slice(2)) {
+        if (path.startsWith('-')) continue;
+        if (!existsSync(path)) {
+            console.log(`File not found: ${path}`);
+            continue;
+        }
+        const absolute = presolve(path);
+        const url = `${address}${pathToURL(absolute)}`;
+        open(url);
+    }
+};
+
+get(`${address}/health`, () => {
+    // server is already running
+    openArgs();
+    process.exit(0);
+}).on('error', () => {
+    // server is not running so we start it
+    server.listen(config.port, async () => {
+        console.log(`App is listening on port ${config.port}!`);
+        openArgs();
+    });
+});
