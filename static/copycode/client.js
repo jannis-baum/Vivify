6df00d1dc7dc90dd6be62ca8a5d9fe3ff342c631
Jason Fairchild <jason@fairchild.au>
2025-02-20 11:59:33 +1000

    WIP: Copy button callbacks

--- static/copycode/client.js
+++ static/copycode/client.js
@@ -1,5 +1,19 @@
 var clipboard = new ClipboardJS('.copy-button');
 
 //TODO: Implement success callback
+clipboard.on('success', function (e) {
+    console.log('Copy success!');
+    console.info('Trigger:', e.trigger);
+    const btn = e.trigger;
+    btn.style.fill = '#00ff00';
+    setTimeout(() => {
+        btn.style.fill = '#0000ff';
+    }, 2000);
+});
 
 //TODO: Implement failure callback
+clipboard.on('error', function (e) {
+    console.log('Copy failed!');
+    console.error('Trigger:', e.trigger);
+});
+
