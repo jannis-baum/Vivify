1cbbbb2087848af591a3d072c28bc3b2d10cea3d
Jannis Baum <jannis.baum@gmail.com>
2025-08-07 18:27:50 +0200

    chore(#231): adjust naming in release script

--- .github/release.sh
+++ .github/release.sh
@@ -1,26 +1,30 @@
 #!/bin/sh
 
+# ensure main branch
 if [ "$(git rev-parse --abbrev-ref HEAD)" != "main" ]; then
-    echo "Releases should only be made from branch main"
+    echo "Releases should only be made from branch main" >&2
     exit 1
 fi
 
-if [ "$1" != "minor" -a "$1" != "major" ]; then
-    echo "Specify increment minor|major."
+# ensure arg
+if [ "$1" != "patch" -a "$1" != "minor" ]; then
+    echo "Specify increment patch|minor." >&2
     exit 1
 fi
 
-version="v0.0.1"
+# base & previous version
+version="v0.1.0"
 prev=$(git tag --sort=version:refname | tail -1)
 
-if [ -n "$prev" ]; then
-    prefix=$(cut -d. -f1 <<< $prev)
-    major=$(cut -d. -f2 <<< $prev)
+# increment
+if [[ -n "$prev" ]]; then
+    major=$(cut -d. -f1 <<< $prev)
+    minor=$(cut -d. -f2 <<< $prev)
 
-    [ "$1" = "minor" ] && minor=$(bc <<< "$(cut -d. -f3 <<< $prev) + 1")
-    [ "$1" = "major" ] && major=$(bc <<< "$major + 1") && minor="0"
+    [[ "$1" = "patch" ]] && patch=$(bc <<< "$(cut -d. -f3 <<< $prev) + 1")
+    [[ "$1" = "minor" ]] && minor=$(bc <<< "$minor + 1") && patch="0"
 
-    version="$prefix.$major.$minor"
+    version="$major.$minor.$patch"
 fi
 
 git tag -s -a $version
