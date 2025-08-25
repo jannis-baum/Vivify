b82efe5d33db5886a87c64fa3a1e5baa789202ae
Tuure Piitulainen <tuure.piitulainen@gmail.com>
2025-08-25 03:14:30 +0300

    feat(#141): handle all sources of icons
    Icons can be set one of the following ways:
    
    1. octicon
    2. filepath to an svg file
        - full path
        - full path with tilde
        - relative path (relative to Vivify config dir)
    3. inline svg
        - unfortunately quite annoying in json format, but works when
          escaping quotes
    
    Problem with custom icons, such as from lucide.dev:
        - octicons work best with `fill="currentColor"`,
        - but lucide icons unwantedly fill the entire icon with that style
            - these would prefer `fill="none" stroke="currentColor"`,
              but octicons work badly this way.
    
    However, looking at the set of icons, I would greatly prefer if we
    switched primary icon providers from octicon to lucide (or something
    else, if there are even better alternatives). We'll consider that in a
    followup issue.

--- src/config.ts
+++ src/config.ts
@@ -72,9 +72,10 @@ const getFileContents = (
     return getFileContent(paths);
 };
 
+let configBaseDir: string | undefined = undefined;
+
 const config = ((): Config => {
     let config = undefined;
-    let configBaseDir = undefined;
     // greedily find config
     for (const cp of configPaths) {
         if (!fs.existsSync(cp)) continue;
@@ -110,6 +111,7 @@ const config = ((): Config => {
     return config;
 })();
 
+export { config, configBaseDir };
 export default config;
 
 export const address = `http://localhost:${config.port}`;
