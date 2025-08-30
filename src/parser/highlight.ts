9028c320fa6e3694af1607bbe959af8a3da18a78
Jason Fairchild <jason@fairchild.au>
2025-08-30 13:58:45 +1000

    fix(#234): Default highlight lang to plaintext

--- src/parser/highlight.ts
+++ src/parser/highlight.ts
@@ -2,15 +2,16 @@ import hljs from 'highlight.js';
 
 export default function highlight(str: string, lang: string) {
     let content = str;
-    if (lang) {
-        if (hljs.getLanguage(lang)) {
-            try {
-                content = hljs.highlight(content, {
-                    language: lang,
-                    ignoreIllegals: true,
-                }).value;
-            } catch {}
-        }
+    if (!lang) {
+        lang = 'text';
+    }
+    if (hljs.getLanguage(lang)) {
+        try {
+            content = hljs.highlight(content, {
+                language: lang,
+                ignoreIllegals: true,
+            }).value;
+        } catch {}
     }
     return `<pre class="language-${lang}"><code>${content}</code></pre>`;
 }
