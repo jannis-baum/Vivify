9074ec919b4559b95e674f0b7491738c99f84da4
Giovanni Cerqui | NET GmbH <g.cerqui@net-gmbh.com>
2026-02-04 08:04:25 +0100

    chore: run viv-debug via ts-node bin instead of loader
    Uses node_modules/ts-node/dist/bin-esm.js to avoid loader resolution
    issues. This keeps debug server running from src/app.ts and enables
    WebSocket path logging in %TEMP%/vivify-server.log.

--- build/windows/viv-debug.cmd
+++ build/windows/viv-debug.cmd
@@ -8,7 +8,7 @@ set "INSTALL_DIR=%~dp0..\..\"
 set "LOG_PATH=%TEMP%\vivify-server.log"
 set "OUT_LOG=%TEMP%\vivify-server.out.log"
 set "ERR_LOG=%TEMP%\vivify-server.err.log"
-set "TS_NODE_LOADER=%INSTALL_DIR%node_modules\ts-node\esm.mjs"
+set "TS_NODE_BIN=%INSTALL_DIR%node_modules\ts-node\dist\bin-esm.js"
 
 echo [%DATE% %TIME%] viv-debug starting > "%LOG_PATH%"
 
@@ -23,8 +23,8 @@ if not exist "%INSTALL_DIR%src\app.ts" (
     exit /b 1
 )
 
-if not exist "%TS_NODE_LOADER%" (
-    echo [%DATE% %TIME%] ERROR: ts-node loader not found at %TS_NODE_LOADER% >> "%LOG_PATH%"
+if not exist "%TS_NODE_BIN%" (
+    echo [%DATE% %TIME%] ERROR: ts-node bin not found at %TS_NODE_BIN% >> "%LOG_PATH%"
     echo [%DATE% %TIME%] Run: yarn install (or npm install) in %INSTALL_DIR% >> "%LOG_PATH%"
     exit /b 1
 )
@@ -35,8 +35,8 @@ set "NODE_ENV=development"
 
 pushd "%INSTALL_DIR%"
 
-rem Run server from source (uses ts-node loader) and capture stdout/stderr
-start /b "" node --loader "%TS_NODE_LOADER%" "%INSTALL_DIR%src\app.ts" %* > "%OUT_LOG%" 2> "%ERR_LOG%"
+rem Run server from source (ts-node CLI) and capture stdout/stderr
+start /b "" node "%TS_NODE_BIN%" "%INSTALL_DIR%src\app.ts" %* > "%OUT_LOG%" 2> "%ERR_LOG%"
 
 echo [%DATE% %TIME%] viv-debug launched ts-node server >> "%LOG_PATH%"
 
